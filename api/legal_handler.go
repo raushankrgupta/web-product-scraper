a9c8fe34d2ba7d3ea7bfc7a87cefa43f1e0c27ae
raushankrgupta <raushankrgupta26@gmail.com>
2026-01-20 15:34:53 +0530

    bug fix

--- api/legal_handler.go
+++ api/legal_handler.go
@@ -56,8 +56,8 @@ Upon deletion, all your personal data and uploaded images are permanently remove
 ## 5. Contact Us
 If you have questions or comments about this policy, you may contact us at:
 - Email: %s
-- Phone: %s
-`, config.ContactEmail, config.ContactEmail, config.ContactPhone)
+- Email: %s
+`, config.ContactEmail, config.ContactEmail)
 
 	utils.RespondJSON(w, http.StatusOK, map[string]string{
 		"content": policy,
